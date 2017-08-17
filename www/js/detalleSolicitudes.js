9958a2ea877e7d0a2538d75b503326a27003a0ec
franc540 <francfox540@gmail.com>
2017-08-17 16:05:35 -0600

    update

--- www/js/detalleSolicitudes.js
+++ www/js/detalleSolicitudes.js
@@ -1,7 +1,7 @@
 var base_url="http://repuestosautoweb.com/";
 
 function getSolicitudes(id_venta_repuesto){
-var dataString = base_url + "sistema/API/getSolicitudesxventa/"+id_venta_repuesto;
+  var dataString = base_url + "sistema/API/getSolicitudesxventa/"+id_venta_repuesto;
   $.ajax({
     type: "POST",
     url: dataString,
@@ -10,20 +10,48 @@ var dataString = base_url + "sistema/API/getSolicitudesxventa/"+id_venta_repuest
 
       console.log(data);
       var dataArray = JSON.parse(data);
-  for(i in dataArray.solicitudes_nuevas){
-    item = dataArray.solicitudes_nuevas[i];
+      for(i in dataArray.solicitudes_nuevas){
+        item = dataArray.solicitudes_nuevas[i];
     //alert(item.nombre);
 
-    htmltoPrint = "<div class='item'><div class='table-responsive'><table><thead><tr><th>Marca</th><th>Modelo</th><th>Año</th><th>Repuestos</th></tr></thead><tbody><tr class='clickable-row' data-href='foobar.com'><td>" + item.marca + "</td><td>" + item.modelo + "</td><td>" + item.anio + "</td><td>" + item.pieza + "</td></tr></tbody></table><br><p>Descripción</p><span>" + item.descripcion + "</span></div><div class='preview-container' style='background-image: url( " + base_url + item.imagen + ");'></div><div><p>Comentarios</p><span>" + item.mensaje + "</span></div><br /><div class='respuesta-container'><p>Respuesta</p><table class='table-respuesta'><tr><td><span>Si</span><label for='respuesta' class='glyphicon glyphicon-ok text-yellow'></label><input type='radio' name='respuesta' id='respuesta' value='S'></td><td><span>No</span><label for='respuesta_0' class='glyphicon glyphicon-remove text-yellow'></label><input type='radio' name='respuesta' id='respuesta' value='N'></td></tr><tr><td><p>Precio</p> <input class='input-control id_solicitud_repuesto' value='"+item.id_solicitud_repuesto+"'' type='hidden'>  <input class='input-control id_solicitud_detalle' value='"+item.id_solicitud_detalle+"'' type='hidden'>    <input class='input-control precio'  type='number'></td><td><p>Garantía</p><input class='input-control garantia'  type='number'></td><td><p>Tiempo</p><select name=\"tiempo\" style=\"width:74px\" class=\"input-control tiempo\"><option value=\"dias\">Dias</option><option value=\"meses\">Meses</option></select></td></tr><tr><td><input type='button' value=\"Enviar\" class=\"btn-red boton\"> </td><tr></table></div><div class='clearfix'></div>      </div>";
+   // htmltoPrint = "<div class='item'><div class='table-responsive'><table><thead><tr><th>Marca</th><th>Modelo</th><th>Año</th><th>Repuestos</th></tr></thead><tbody><tr class='clickable-row' data-href='foobar.com'><td>" + item.marca + "</td><td>" + item.modelo + "</td><td>" + item.anio + "</td><td>" + item.pieza + "</td></tr></tbody></table><br><p>Descripción</p><span>" + item.descripcion + "</span></div><div class='preview-container' style='background-image: url( " + base_url + item.imagen + ");'></div><div><p>Comentarios</p><span>" + item.mensaje + "</span></div><br /><div class='respuesta-container'><p>Respuesta</p><table class='table-respuesta'><tr><td><span>Si</span><label for='respuesta' class='glyphicon glyphicon-ok text-yellow'></label><input type='radio' name='respuesta' id='respuesta' value='S'></td><td><span>No</span><label for='respuesta_0' class='glyphicon glyphicon-remove text-yellow'></label><input type='radio' name='respuesta' id='respuesta' value='N'></td></tr><tr><td><p>Precio</p> <input class='input-control id_solicitud_repuesto' value='"+item.id_solicitud_repuesto+"'' type='hidden'>  <input class='input-control id_solicitud_detalle' value='"+item.id_solicitud_detalle+"'' type='hidden'>    <input class='input-control precio'  type='number'></td><td><p>Garantía</p><input class='input-control garantia'  type='number'></td><td><p>Tiempo</p><select name=\"tiempo\" style=\"width:74px\" class=\"input-control tiempo\"><option value=\"dias\">Dias</option><option value=\"meses\">Meses</option></select></td></tr><tr><td><input type='button' value=\"Enviar\" class=\"btn-red boton\"> </td><tr></table></div><div class='clearfix'></div>      </div>";
+   htmltoPrint= ""+
+   "<div class='item'>"+
+   "<div class='table-responsive'>"+
+   "<table>"+
+   "<thead>"+
+   "<tr><th>Marca</th><th>Modelo</th><th>Año</th><th>Repuestos</th></tr>"+
+   "</thead>"+
+   "<tbody>"+
+   "<tr class='clickable-row' ><td>" + item.marca + "</td><td>" + item.modelo + "</td><td>" + item.anio + "</td><td>" + item.pieza + "</td></tr>"+
+   "</tbody>"+
+   "</table>"+
+   "<br><p>Descripción</p>"+
+   "<span>" + item.descripcion + "</span>"+
+   "</div>"+
+   "<div class='preview-container' style='background-image: url( " + base_url + item.imagen + ");'></div>"+
+   "<div><p>Comentarios</p><span>" + item.mensaje + "</span></div>"+
+   "<br /><div class='respuesta-container'><p>Respuesta</p>"+
+   "<table class='table-respuesta'>"+
+   "<tr>"+
+   "<td><span>Si</span><label for='respuesta' class='glyphicon glyphicon-ok text-yellow'></label><input type='radio' name='respuesta' id='respuesta' value='S'></td>"+
+   "<td><span>No</span><label for='respuesta_0' class='glyphicon glyphicon-remove text-yellow'></label><input type='radio' name='respuesta' id='respuesta' value='N'></td>"+
+   "</tr>"+
+   "<tr><td><input type='button' value=\"Enviar\" class=\"btn-red boton\" id_solicitud_detalle='"+item.id_solicitud_detalle+"' id_solicitud_repuesto='"+item.id_solicitud_repuesto+"' > </td><tr></table></div><div class='clearfix'></div></div>";
+   
+   $("#lista-items").append(htmltoPrint);
+   
+   
+ }
+ if(dataArray.solicitudes_nuevas==false){
+  $("#lista-items").append('<b>No se poseen solicitudes nuevas</b>');
+  
+}
+setFooter();
+loading_hide();
 
-    $("#lista-items").append(htmltoPrint);
-    setFooter();
-    loading_hide();
-  }
-
-
-    }
-  });
+}
+});
 
 };
 
@@ -47,10 +75,10 @@ $(document).on('click','#respuesta',function(){
 });
 
 $(document).on('click','.boton',function(){
-    var garantia = $(this).parent().parent().parent().find(".garantia").val()+' '+$(this).parent().parent().parent().find(".tiempo").val();
-    var precio = $(this).parent().parent().parent().find(".precio").val();
-    var id_solicitud_detalle = $(this).parent().parent().parent().find(".id_solicitud_detalle").val();
-    var id_solicitud_repuesto = $(this).parent().parent().parent().find(".id_solicitud_repuesto").val();
+  var garantia = $(this).parent().parent().parent().find(".garantia").val()+' '+$(this).parent().parent().parent().find(".tiempo").val();
+  var precio = $(this).parent().parent().parent().find(".precio").val();
+  var id_solicitud_detalle = $(this).attr("id_solicitud_detalle");
+  var id_solicitud_repuesto = $(this).attr("id_solicitud_repuesto");
 /*
     if(!$.isNumeric(garantia)){
       alert("Por favor ingrese en número la cantidad de meses");
@@ -58,30 +86,32 @@ $(document).on('click','.boton',function(){
       // $(this).parent().parent().parent().find(".garantia").focus();
     }
     */
-
-    if(!$.isNumeric(precio)){
-      alert("Por favor ingresar un valor sin comas y signo de moneda");
-        return;
+    index=$(this).index(".boton");
+   // if(!$.isNumeric(precio)){
+     // alert("Por favor ingresar un valor sin comas y signo de moneda");
+    //    return;
       // $(this).parent().parent().parent().find(".garantia").focus();
+   // }
+   respuesta=$(".table-respuesta:eq("+index+")").find('[name="respuesta"]:checked').val();
+   respuesta=(respuesta=="S"?"Si":"No");
+   
+   var dataString = base_url + "sistema/API/setDatos/"+id_solicitud_detalle+"/"+id_solicitud_repuesto+"/0"+"/0";
+   $.ajax({
+    type: "POST",
+    url: dataString,
+    data:{'precio':precio,'garantia':garantia,'respuesta':respuesta},
+    beforeSend: function(){ $("#loginMsg").html('ObteniendoDatos');},
+    success: function(data){
+
+      window.location.href = "detalleSolicitud.html";
+
+
     }
-
-    var dataString = base_url + "sistema/API/setDatos/"+id_solicitud_detalle+"/"+id_solicitud_repuesto+"/"+precio+"/"+garantia;
-      $.ajax({
-        type: "POST",
-        url: dataString,
-        data:{'precio':precio,'garantia':garantia},
-        beforeSend: function(){ $("#loginMsg").html('ObteniendoDatos');},
-        success: function(data){
-
-          window.location.href = "detalleSolicitud.html";
-
-
-        }
-      });
+  });
 
 
 
-});
+ });
 
 
 document.addEventListener("deviceready", onDeviceReady, false);
@@ -92,11 +122,11 @@ document.addEventListener("deviceready", onDeviceReady, false);
     setFooter();
     loading_show();
     //console.log(window.localStorage.getItem("id_venta_repuesto"));
-   getSolicitudes(window.localStorage.getItem("id_venta_repuesto"));
+    getSolicitudes(window.localStorage.getItem("id_venta_repuesto"));
 
      //getSolicitudes(window.localStorage.getItem("id_venta_repuesto"))
 
    }
-$(document).ready(function(){
+   $(document).ready(function(){
   //onDeviceReady();
 });
